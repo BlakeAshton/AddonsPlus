a4d4a7305f91635f64f9557fabd791c94e4ab44d
BlakeAshton <taureremixwow@gmail.com>
2026-03-10 19:04:08 +0800

    Add files via upload

---
+++ enUS.lua
@@ -0,0 +1,9 @@
+local L = {}
+
+L["ADDON_NAME"] = "MyAddon"
+L["WELCOME_MESSAGE"] = "Welcome to MyAddon!"
+L["CONFIG_TITLE"] = "MyAddon Configuration"
+L["ENABLE_FEATURE"] = "Enable Feature"
+L["DISABLE_FEATURE"] = "Disable Feature"
+
+return L
